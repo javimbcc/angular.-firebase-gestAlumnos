b669825e9e3b0fd4faa527cd790e0098a391f518
DESKTOP-HGE6DGN\javi <csi22-jmennza@colegioaltair.net>
2022-11-30 10:17:55 +0100

    solo flata una cosita en los detalles para que se terminen de mostrar

--- src/app/components/detalles-mascotas/detalles-mascotas.component.ts
+++ src/app/components/detalles-mascotas/detalles-mascotas.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Mascota } from 'src/app/interfaces/mascota';
+import { MascotasServiceService } from 'src/app/services/mascotas-service.service';
 
 @Component({
   selector: 'app-detalles-mascotas',
   templateUrl: './detalles-mascotas.component.html',
-  styleUrls: ['./detalles-mascotas.component.css']
+  styleUrls: ['./detalles-mascotas.component.css'],
 })
 export class DetallesMascotasComponent implements OnInit {
+  mascota?: Mascota;
+  constructor(private msService: MascotasServiceService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async getInfoContacto() {
+    this.msService.getUnaMascota(this.mascota!);
   }
 
+  ngOnInit(): void {
+    this.getInfoContacto();
+  }
 }
